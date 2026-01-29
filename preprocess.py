9229e2c7e35d86b1a5c7e634e86a63c428385dc9
Manan <imananop@gmail.com>
2026-01-29 23:31:41 +0530

    json

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,8 @@
-def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"):
+import json
 
+def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"):
+    with open(raw_file_path, encoding="utf-8") as file:
+        json.load(file)
 
 if __name__ == "__main__":
     process_posts("data/raw_posts.json", "data/processed_posts.json")
