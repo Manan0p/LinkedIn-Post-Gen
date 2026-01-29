60b5c002e29d73256d0b9b7a35b75d750251d6f0
Manan <imananop@gmail.com>
2026-01-29 23:30:30 +0530

    files path

---
+++ preprocess.py
@@ -0,0 +1,5 @@
+def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"):
+
+
+if __name__ == "__main__":
+    process_posts("data/raw_posts.json", "data/processed_posts.json")
