91ed5f68f19b0c957eab10f367eab7f2a7b0f6ed
Manan <imananop@gmail.com>
2026-01-31 00:19:02 +0530

    model set

--- llm_helper.py
+++ llm_helper.py
@@ -4,4 +4,8 @@ import os
 
 load_dotenv()
 
-llm = ChatGroq(groq_api_key = os.getenv("GROQ_API_KEY"), model_name = "llma")
+llm = ChatGroq(groq_api_key = os.getenv("GROQ_API_KEY"), model_name = "llama-3.3-70b-versatile")
+
+if __name__ == "__main__":
+    response = llm.invoke("What are the two main ingredients in samosa")
+    print(response.content)
