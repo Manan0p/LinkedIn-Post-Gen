2695d970464a42583255a8205486e0af1a9895d5
Manan <imananop@gmail.com>
2026-02-01 15:24:23 +0530

    loaded tags

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st
+from few_shot import FewShotPosts
 
 def main():
     st.title("LinkedIn Post Generator")
     col1, col2, col3 = st.columns(3)
+    fs = FewShotPosts()
     with col1:
-        st.selectbox("Title", options=[])
+        st.selectbox("Title", options=[fs.get_tags()])
 
 if __name__ == "__main__":
     main()
