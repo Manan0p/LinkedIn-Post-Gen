15ce7d1e361dc7ae254c2ce6d45dcbf40097d0f2
Manan <imananop@gmail.com>
2026-02-01 15:21:02 +0530

    col 1

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 def main():
     st.title("LinkedIn Post Generator")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.selectbox("Title", options=[])
 
 if __name__ == "__main__":
     main()
