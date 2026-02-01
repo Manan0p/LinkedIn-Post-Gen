6d9274762de123f43980c8af6530c89b2fe10fc4
Manan <imananop@gmail.com>
2026-02-01 16:06:34 +0530

    length

--- post_generator.py
+++ post_generator.py
@@ -1,5 +1,13 @@
 from llm_helper import llm
 
+def get_length_str(length):
+    if length == "Short":
+        return "1 to 5 lines"
+    elif length == "Medium":
+        return "6 to 10 lines"
+    else:
+        return "more than 10 lines"
+
 def generate_post(length, language, topic):
     prompt = ''''''
     response = llm.invoke(prompt)
