2a702fe69cbc1030790f3275509157efd8613ad1
Manan <imananop@gmail.com>
2026-02-01 16:29:38 +0530

    removed lang

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from few_shot import FewShotPosts
 from post_generator import generate_post
 
 length_options = ["Short", "Medium", "Long"]
-language_options = ["English", "Hinglish", "Hindi"]
+language_options = ["English", "Hinglish"]
 
 def main():
     st.title("LinkedIn Post Generator")
