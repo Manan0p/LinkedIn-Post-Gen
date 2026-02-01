645718c1d494ac7189ddf3d33c56b7f025cde5b8
Manan <imananop@gmail.com>
2026-02-01 14:35:28 +0530

    loadpost

--- few_shot.py
+++ few_shot.py
@@ -1,6 +1,11 @@
-
+import json
 
 class FewShotPosts:
     def __init__(self, filepath="data/processed_posts.json"):
         self.df = None
         self.unique_tags = None
+
+    def load_post(self,file_path):
+        with open(file_path, encoding="utf-8") as f:
+            posts = json.load(f)
+        pass
